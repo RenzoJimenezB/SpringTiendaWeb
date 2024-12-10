647fa6584a23d7251264cfb29e5200e52799a393
RenzoJimenezB <adrianrjb@outlook.es>
2024-12-10 00:06:14 -0500

    loadedUserByUsername

--- src/main/java/pe/edu/tecsup/tienda/repositories/UserRepository.java
+++ src/main/java/pe/edu/tecsup/tienda/repositories/UserRepository.java
@@ -1,6 +1,9 @@
 package pe.edu.tecsup.tienda.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.userdetails.UserDetails;
 import pe.edu.tecsup.tienda.entities.User;
 
 import java.util.List;
@@ -10,4 +13,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByLastName(String lastName);
 
     List<User> findByState(Integer state);
+
+    @Query("SELECT u FROM User u WHERE u.email=:email")
+    UserDetails loadUserByUsername(@Param("email") String email);
 }

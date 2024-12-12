4441594b130275c0db7ca7a1472967cf051f4ed3
RenzoJimenezB <adrianrjb@outlook.es>
2024-12-12 11:53:31 -0500

    SecurityConfig update

--- src/main/java/pe/edu/tecsup/tienda/config/SecurityConfig.java
+++ src/main/java/pe/edu/tecsup/tienda/config/SecurityConfig.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 /*
 class RawPasswordEncoder implements PasswordEncoder {
@@ -38,6 +41,21 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
 //        return new RawPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests ->
+                        requests.requestMatchers("/", "/webjars/**", "/css/**", "/error/**")
+                                .permitAll()
+                                .requestMatchers("/admin/**")
+                                .hasAnyAuthority("Administrador"))
+                .formLogin(form -> form.loginProcessingUrl("/authenticate")
+                        .loginPage("/login").permitAll())
+                .logout(logout -> logout.logoutUrl("/logout")
+                        .logoutSuccessUrl("/login")).csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
     }
 
     @Autowired
